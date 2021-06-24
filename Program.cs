642b819d6afb7e9c4c0eef0087c6601e4d2aa7c4
SharfuddinMohammad <86398016+SharfuddinMohammad@users.noreply.github.com>
2021-06-24 16:08:24 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Xyz
+{
+    class Program
+    {
+        string color;
+        int maxSpeed;
+
+        static void Main(string[] args)
+        {
+            Program myObj = new Program();
+            myObj.color = "read";
+            myObj.maxSpeed = 200;
+            Console.WriteLine(myObj.color);
+            Console.WriteLine(myObj.maxSpeed);
+        }
+    }
+}
